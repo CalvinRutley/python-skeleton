723e03442c37c34972243ceb67f8dc6eb4d4150d
Calvin Rutley <crutley@MacBook-Pro.local>
2018-10-18 15:08:53 -0700

    update q1 to provide a slow approach

--- q1.py
+++ q1.py
@@ -5,9 +5,15 @@ import numpy as np
 # modify this function, and create other functions below as you wish
 def question01(portfolios):
   # modify and then return the variable below
+  max = 0
+  answer = 0 
   for i in range(0, len(portfolios)-1):
-    answer = portfolios[i] ^ portfolios[i+1]
+    for j in range(0, len(portfolios)-1):
+      Xor = portfolios[i] ^ portfolios[j]
+      if (Xor > max):
+        max = Xor
+        answer = max
   return answer
 
-port = [15,7]
+port = [15,7,23,43,45,5,5,4,64,56,6,7,8,3,5,7]
 print(question01(port))
