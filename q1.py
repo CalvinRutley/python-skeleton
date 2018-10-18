9fce209d62469f1e1a16ce21f4b7190db06815ce
Calvin Rutley <crutley@MacBook-Pro.local>
2018-10-18 14:31:56 -0700

    test

--- q1.py
+++ q1.py
@@ -5,5 +5,9 @@ import numpy as np
 # modify this function, and create other functions below as you wish
 def question01(portfolios):
   # modify and then return the variable below
-  answer = 0
+  for i in range(0, len(portfolios)-1):
+    answer = portfolios[i] ^ portfolios[i+1]
   return answer
+
+port = [15,7]
+print(question01(port))
