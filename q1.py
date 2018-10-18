2b17710a6bf07a98113abfe1393b47e8143d1466
Calvin Rutley <crutley@MacBook-Pro.local>
2018-10-18 16:36:25 -0700

    Updates q1

--- q1.py
+++ q1.py
@@ -5,15 +5,9 @@ import numpy as np
 # modify this function, and create other functions below as you wish
 def question01(portfolios):
   # modify and then return the variable below
-  max = 0
-  answer = 0 
-  for i in range(0, len(portfolios)-1):
-    for j in range(0, len(portfolios)-1):
-      Xor = portfolios[i] ^ portfolios[j]
-      if (Xor > max):
-        max = Xor
-        answer = max
+  answer = 0
+  for i in range(16)[::-1]:
+    answer <<= 1
+    start = {port >> i for port in portfolios}
+    answer += any(answer^1 ^ s in start for s in start)
   return answer
-
-port = [15,7,23,43,45,5,5,4,64,56,6,7,8,3,5,7]
-print(question01(port))
