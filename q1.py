6356c040a594751ed0561d2100817fa7a8e75b6b
Calvin Rutley <crutley@MacBook-Pro.local>
2018-10-20 10:20:02 -0700

    o(n) q1

--- q1.py
+++ q1.py
@@ -2,51 +2,17 @@
 
 import numpy as np
 
-class TrieNode():
-  zero = None
-  one = None
-  value = None
-
 # modify this function, and create other functions below as you wish
 def question01(portfolios):
-  root = TrieNode()
-  bits = range(31,-1,-1)
-
-  for p in portfolios:
-    current = root
-    for i in bits:
-      m = 1 << i
-      mp = p & m
-
-      if mp is 0:
-        if not current.zero:
-          current.zero = TrieNode()
-        current = current.zero
-      else:
-        if not current.zero:
-          current.one = TrieNode()
-        current = current.one
-    current.value = p
-  
-  answer = 0
-  for p in portfolios:
-    current = root
-    for i in bits:
-      m = 1 << i
-      mp = p & m
-
-      if mp is 0:
-        if current.one:
-          current = current.one
-        else:
-          current = current.zero
-      else:
-        if current.zero:
-          current = current.zero
-        else:
-          current = current.one
-    temp_max = p ^ current.value
-    if temp_max > answer:
-      answer = temp_max
-  
-  return answer
+  ans, m = 0, 0
+  for i in range(31,-1,-1):
+    temp = ans | 1 << i
+    m = m | 1 << i
+    bits = set()
+    for p in portfolios:
+      bits.add(p & m)
+    for bit in bits:
+      if bit ^ temp in bits:
+        ans = temp
+        break
+  return ans
