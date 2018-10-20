470d6d8d9f9ba5a785b3ccffe48e1f1e70141c11
Calvin Rutley <crutley@MacBook-Pro.local>
2018-10-20 12:15:00 -0700

    quicker q1

--- q1.py
+++ q1.py
@@ -4,15 +4,16 @@ import numpy as np
 
 # modify this function, and create other functions below as you wish
 def question01(portfolios):
-  ans, m = 0, 0
-  for i in range(31,-1,-1):
-    temp = ans | 1 << i
+  _ans = 0
+  m = 0
+  for i in range(31, -1, -1):
     m = m | 1 << i
-    bits = set()
+    _set = set()
+    tmp = _ans | 1 << i
     for p in portfolios:
-      bits.add(p & m)
-    for bit in bits:
-      if bit ^ temp in bits:
-        ans = temp
+      prefix = p & m
+      if prefix ^ tmp in _set:
+        _ans = tmp
         break
-  return ans
+      _set.add(prefix)
+  return _ans
